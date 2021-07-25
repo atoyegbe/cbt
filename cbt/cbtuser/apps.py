abf86ea3a6aa0329d95cf1916d5bf030e7240432
Victor <olowofesovictor@gmail.com>
2021-07-25 15:33:15 +0100

    CBT User model

---
+++ cbt/cbtuser/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CbtuserConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'cbtuser'
---
+++ cbt/cbtuser/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class CBTUser(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_of_birth = models.DateField()
+    first_name = models.CharField(max_length=255, blank=False, null=False)
+    last_name = models.CharField(max_length=255, blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
