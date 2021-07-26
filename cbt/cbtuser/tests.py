bd87bb9f543614b1ed2deaca92dcc871b9810a99
Victor <olowofesovictor@gmail.com>
2021-07-26 20:53:05 +0100

    logout test

--- cbt/cbtuser/tests.py
+++ cbt/cbtuser/tests.py
@@ -4,6 +4,7 @@ from django.test import TestCase, Client
 from django.contrib.auth.models import User
 from django.urls import reverse
 from rest_framework import status
+from rest_framework.test import APIClient
 
 from cbtuser.views import generate_username
 
@@ -14,7 +15,7 @@ client = Client(enforce_csrf_checks=False) # in testmode this is False
 class CreateCBTUserTest(TestCase):
     """ Test module for create CBTUser API """
 
-    def setUp(self):
+    def setUp(self) -> None:
         self.valid_payload = {
             'date_of_birth': '1948-11-29',
             'first_name': "Dianna",
@@ -29,7 +30,7 @@ class CreateCBTUserTest(TestCase):
         }
 
 
-    def test_generate_username(self):
+    def test_generate_username(self) -> None:
         self.assertTrue(type(generate_username("adele loaves")) == str) 
         self.assertTrue(len(generate_username("Matt Ha"))> 1)
         try:
@@ -39,7 +40,7 @@ class CreateCBTUserTest(TestCase):
         finally:
             self.assertTrue(True,True)
 
-    def test_create_valid_cbtuser(self):
+    def test_create_valid_cbtuser(self) -> None:
         response = client.post(
             path = reverse('create_cbtuser'),
             data = json.dumps(self.valid_payload),
@@ -48,7 +49,7 @@ class CreateCBTUserTest(TestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
 
-    def test_create_invalid_cbtuser(self):
+    def test_create_invalid_cbtuser(self) -> None:
         response = client.post(
             path  = reverse('create_cbtuser'),
             data = json.dumps(self.invalid_payload),
@@ -77,7 +78,7 @@ class LoginCBTUserTest(TestCase):
             'username': self.user.username,
         }
 
-    def test_valid_login_cbtuser(self):
+    def test_valid_login_cbtuser(self) -> None:
         response = client.post(
             path = reverse('login_cbtuser'),
             data = json.dumps(self.valid_payload),
@@ -85,7 +86,7 @@ class LoginCBTUserTest(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_invalid_login_cbtuser(self):
+    def test_invalid_login_cbtuser(self) -> None:
         response = client.post(
             path = reverse('login_cbtuser'),
             data = json.dumps(self.invalid_payload),
@@ -98,3 +99,27 @@ class LoginCBTUserTest(TestCase):
             content_type = 'application/json'
         )
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class LogoutCBTUserTest(TestCase):
+        
+    def setUp(self) -> None:
+        self.user = User.objects.create_user(username = "abc123", 
+                                            password= "password123", 
+                                            email="dianna@cbt.com")
+        self.user.save()
+        
+
+    def test_logout_cbtuser(self) -> None:
+        response = client.post(
+            path = reverse('login_cbtuser'),    
+            data = json.dumps({'username': self.user.username,
+                                'password': 'password123'}),
+            content_type = 'application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+        c = APIClient()
+        c.credentials(HTTP_AUTHORIZATION = "Token "+response.json().get("token"))
+        response = c.get(path = reverse('logout_cbtuser'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- cbt/cbtuser/urls.py
+++ cbt/cbtuser/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('create/', views.create, name="create_cbtuser"),
     path('login/', views.login_cbtuser, name="login_cbtuser"),
-    path('aaa/', views.logout_cbtuser, name="logout_cbtuser"),
+    path('logout/', views.logout_cbtuser, name="logout_cbtuser"),
 ]
